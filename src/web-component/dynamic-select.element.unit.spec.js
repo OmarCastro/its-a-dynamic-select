a04c49433316ab9c23920fb2160cc191a39d49d8
OmarCastro <omar.castro.360@gmail.com>
2025-08-09 20:56:30 +0100

    test: update test usage

--- src/web-component/dynamic-select.element.unit.spec.js
+++ src/web-component/dynamic-select.element.unit.spec.js
@@ -1,4 +1,4 @@
-import { test } from '../../test-utils/unit/test.ts'
+import { test } from '../../test-utils/unit/test.util.js'
 
 test('test example', async ({ step, expect }) => {
   await expect(1).toBe(1)
