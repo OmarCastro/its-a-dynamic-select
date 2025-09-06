626020f73d3b7a9c21c78e64ce63791e912fcedc
OmarCastro <omar.castro.360@gmail.com>
2025-09-06 23:43:55 +0100

    feat: improve data fetch
    - separate initial page data fetch from next page data fetch
    
    - simplify code
    
    - improve documentation

--- src/features/data-loading/fetch-data.js
+++ src/features/data-loading/fetch-data.js
@@ -1,30 +1,45 @@
 import { parseCSVResponse, isCSVResponse } from '../parses-csv-data-response/parse-csv-response.js'
 import { parseJsonResponse, isJsonResponse } from '../parses-json-array-and-object-data-response/parse-json-response.js'
+
+/** @type {WeakMap<HTMLElement, DataLoader>} */
 const dataLoaderData = new WeakMap()
 
 /**
- *
- * @param {HTMLElement} element
+ * @param {HTMLElement} element - target element
+ * @returns {DataLoader} dataloader of element
  */
 export function dataLoaderOf (element) {
-  if (dataLoaderData.has(element)) {
-    return dataLoaderData.get(element)
+  let dataLoader = dataLoaderData.get(element)
+  if (!dataLoader) {
+    dataLoader = createDataLoaderFor(new WeakRef(element))
+    dataLoaderData.set(element, dataLoader)
   }
+  return dataLoader
+}
 
-  const newDataLoader = {
-    fetchData: () => fetchData(element),
-
+/**
+ * Creates a dataloader for an element
+ * @param {WeakRef<HTMLElement>} elementRef - weak reference of element. We do not want to have any strong reference chain pointing to
+ * globally allocated `dataLoaderData`, effectively creating a memory leak
+ * @returns {DataLoader} - created dataloader for element
+ */
+function createDataLoaderFor (elementRef) {
+  const getElement = () => {
+    const element = elementRef.deref()
+    if (!element) { throw new Error('element no longer exists') }
+    return element
+  }
+  return {
+    fetchData: async () => await fetchData(getElement()),
+    fetchNextData: async () => await fetchNextData(getElement()),
     fetchHistory: [],
     currentData: null
   }
-
-  dataLoaderData.set(element, newDataLoader)
-  return newDataLoader
 }
 
 /**
  *
- * @param {HTMLElement} element
+ * @param {HTMLElement} element - target element
  * @param dataToFetch
  */
 function dispatchFetchDataEvent (element, dataToFetch) {
@@ -49,55 +64,108 @@ function dispatchFetchDataEvent (element, dataToFetch) {
 
 /**
  *
- * @param {HTMLElement} element
+ * @param {HTMLElement} element - target element to load data to
  */
 async function fetchData (element) {
   const loader = dataLoaderOf(element)
-  const { currentData } = loader
-  const { lastValue, navigationMode, link } = currentData ?? {}
 
+  const url = getUrlToFetch(element)
   const dataToFetch = {
-    query: '',
-    lastValue: lastValue ?? null,
-    navigationMode,
-    ...(navigationMode === 'link' ? { link } : {})
-
+    query: getQueryValue(element),
+    url
   }
 
   const { event, customResponse, respondWithCalls } = dispatchFetchDataEvent(element, dataToFetch)
   if (respondWithCalls > 0) {
     const response = await customResponse
-    loader.currentData = {
-      optionsData: response
+    loader.currentData = response
+    return loader.currentData
+  }
+
+  if (!event.defaultPrevented && url) {
+    const response = await fetch(url)
+    await parseResponse(element, response, dataToFetch)
+  }
+
+  return loader.currentData
+}
+
+/**
+ *
+ * @param {HTMLElement} element
+ */
+async function fetchNextData (element) {
+  const loader = dataLoaderOf(element)
+  const { currentData } = loader
+
+  const url = getUrlToFetch(element)
+
+  const dataToFetch = (() => {
+    const base = {
+      query: getQueryValue(element),
+      url
     }
-    return
+    if (currentData == null || !currentData.hasMore) {
+      return base
+    }
+    if (currentData.navigationMode === 'link') {
+      return {
+        ...base,
+        url: currentData.href
+      }
+    }
+    if (currentData.navigationMode === 'cursor') {
+      return {
+        ...base,
+      }
+    }
+    return base
+  })()
+
+  const { event, customResponse, respondWithCalls } = dispatchFetchDataEvent(element, dataToFetch)
+  if (respondWithCalls > 0) {
+    const response = await customResponse
+    loader.currentData = response
+    return loader.currentData
   }
 
   const src = element.getAttribute('data-src')
   if (!event.defaultPrevented && src) {
     const response = await fetch(src)
-    if (isValidResponse(response)) {
-      if (isCSVResponse(response)) {
-        loader.currentData = parseCSVResponse(response)
-      } else if (isJsonResponse(response)) {
-        loader.currentData = parseJsonResponse(response)
-      } else {
-        addToFetchHistory(element, dataToFetch, {
-          ok: false,
-          status: response.status,
-          error: 'invalid response'
-        })
-      }
+    await parseResponse(element, response, dataToFetch)
+  }
+
+  return loader.currentData
+}
+
+/**
+ *
+ * @param element
+ * @param response
+ * @param dataToFetch
+ */
+async function parseResponse (element, response, dataToFetch) {
+  const loader = dataLoaderOf(element)
+
+  if (isValidResponse(response)) {
+    if (isCSVResponse(response)) {
+      loader.currentData = await parseCSVResponse(response)
+    } else if (isJsonResponse(response)) {
+      loader.currentData = await parseJsonResponse(response)
     } else {
       addToFetchHistory(element, dataToFetch, {
         ok: false,
         status: response.status,
-        error: `${response.status} HTTP status code`
+        error: 'invalid response'
       })
     }
+  } else {
+    addToFetchHistory(element, dataToFetch, {
+      ok: false,
+      status: response.status,
+      error: `${response.status} HTTP status code`
+    })
   }
-
-  return loader.currentData
 }
 
 /**
@@ -116,6 +184,35 @@ function addToFetchHistory (element, dataToFetch, result) {
 
 /**
  *
+ * @param {HTMLElement} element
+ */
+const getQueryValue = element => element.getAttribute('data-filter') ?? ''
+
+/**
+ *
+ * @param {HTMLElement} element
+ */
+const getDataSource = element => element.getAttribute('data-src') ?? ''
+
+/**
+ *
+ * @param {HTMLElement} element
+ */
+const getUrlToFetch = element => {
+  const src = getDataSource(element)
+  if (!src) {
+    return src
+  }
+  const query = getQueryValue(element)
+  const srcURL = new URL(src, window.location.href)
+  if (query) {
+    srcURL.searchParams.append('q', query)
+  }
+  return srcURL.href
+}
+
+/**
+ *
  * @param {Response} response
  * @returns
  */
@@ -124,8 +221,22 @@ function isValidResponse (response) {
 }
 
 /**
+ * @typedef {object} DataLoader
+ * @property {() => Promise<ParsedResponse|null>} fetchData - fetches data and saves to current data
+ * @property {() => Promise<ParsedResponse|null>} fetchNextData - used for paginated options, fetches next page
+ * @property {*[]} fetchHistory - get fetch history, history is no longer than 128 requests
+ * @property {ParsedResponse|null} currentData - current data for select box
+ */
+
+/**
  * @typedef {object} ParsedResponseBase
- * @property {boolean} hasMore - flag to determine if there is more data after the last element
+ * @property {true} hasMore - flag to determine if there is more data after the last element
+ * @property {{[prop: string]: string}[]} data - options data
+ */
+
+/**
+ * @typedef {object} NoMoreResponse
+ * @property {false} hasMore - flag to determine if there is more data after the last element
  * @property {{[prop: string]: string}[]} data - options data
  */
 
@@ -141,5 +252,5 @@ function isValidResponse (response) {
  */
 
 /**
- * @typedef {ParsedResponseBase | (ParsedResponseBase & (CursorPaginatedParsedResponse | LinkPaginatedParsedResponse))} ParsedResponse
+ * @typedef {NoMoreResponse | (ParsedResponseBase & (CursorPaginatedParsedResponse | LinkPaginatedParsedResponse))} ParsedResponse
  */
