097f6c569c96da812252f7e2338c6764c01d4550
OmarCastro <omar.castro.360@gmail.com>
2025-12-07 15:44:46 +0000

    test: add "dropdown is scrollable if its content overflows the screen" ui tests
    - Also update ui tests in the same file

--- src/features/centers-dropdown-to-screen-on-mobile/dropdown-mobile-centering.ui.spec.js
+++ src/features/centers-dropdown-to-screen-on-mobile/dropdown-mobile-centering.ui.spec.js
@@ -1,14 +1,23 @@
 import { test } from '../../../test-utils/ui/test.util.js'
 import { devices } from '@playwright/test'
+/** @import { Page } from '@playwright/test' */
 
 test.use({
   ...devices['Galaxy S8']
 })
-
-test('dynamic-select dropdown centering visual test', async ({ page, expect }) => {
-  await page.goto('./build/docs/test-page.html')
-  await page.reload()
-  const selectbox = page.locator('.dynamic-select--hello-world')
+/**
+ * @param {Page} page
+ */
+test('dynamic-select dropdown centers on mobile', async ({ page, expect }) => {
+  await page.goto('./build/docs/test-page.html#test-basic-select')
+  const selectbox = page.getByRole('button').filter({ visible: true })
   await selectbox.click()
-  await expect.soft(await page.screenshot()).toMatchSnapshot('dropdown-visible-on-click.png')
+  await expect.soft(await page.screenshot()).toMatchSnapshot('dropdown-centers-on-mobile.png')
+})
+
+test('dynamic-select dropdown is scrollable if its content overflows the screen ', async ({ page, expect }) => {
+  await page.goto('./build/docs/test-page.html#test-group-select')
+  const selectbox = page.getByRole('button').filter({ visible: true })
+  await selectbox.click()
+  await expect.soft(await page.screenshot()).toMatchSnapshot('dropdown-scrollable-on-overflow.png')
 })
