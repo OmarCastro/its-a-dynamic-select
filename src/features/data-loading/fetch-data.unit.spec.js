02fcb00607fe8ece76c810cea01aacdac8aeb7af
OmarCastro <omar.castro.360@gmail.com>
2025-12-17 21:48:20 +0000

    test: add memoization and fetchNextData test

--- src/features/data-loading/fetch-data.unit.spec.js
+++ src/features/data-loading/fetch-data.unit.spec.js
@@ -1,14 +1,17 @@
 import { test } from '../../../test-utils/unit/test.util.js'
 import { dataLoaderOf } from './fetch-data.js'
 
-test('dataLoaderOf - without any defined endpoint return an empty result', async ({ expect, dom }) => {
-  const { document } = dom
-  const { body } = document
-  body.innerHTML = `
-    <div class="test">
-    </div>
-  `
+test('dataLoaderOf - calling on the same element returns the same object', async ({ expect, dom }) => {
+  const { body } = dom.document
+  body.innerHTML = '<div class="test"></div>'
+  const element = body.querySelector('.test')
 
+  expect(dataLoaderOf(element)).toBe(dataLoaderOf(element))
+})
+
+test('dataLoaderOf - without any defined endpoint return an empty result', async ({ expect, dom }) => {
+  const { body } = dom.document
+  body.innerHTML = '<div class="test"></div>'
   const element = body.querySelector('.test')
 
   const data = await dataLoaderOf(element).fetchData()
@@ -17,20 +20,27 @@ test('dataLoaderOf - without any defined endpoint return an empty result', async
 })
 
 test('dataLoaderOf - without a endpoint return an valid result', async ({ expect, dom, fetch }) => {
-  const { document } = dom
-  const { body } = document
-  body.innerHTML = `
-    <div class="test" data-src="/test">
-    </div>
-  `
-
   fetch.mock(/.*test/, Response.json([
     { id: 'sdsd', text: 'sss' }
   ]))
-
+  const { body } = dom.document
+  body.innerHTML = '<div class="test" data-src="/test"></div>'
   const element = body.querySelector('.test')
 
   const data = await dataLoaderOf(element).fetchData()
 
   expect(data).toEqual({ data: [{ id: 'sdsd', text: 'sss' }], hasMore: false })
+})
+
+test('dataLoaderOf - fetching next data when hasMore is false returns an empty result', async ({ expect, dom }) => {
+  const { body } = dom.document
+  body.innerHTML = '<div class="test"></div>'
+
+  const element = body.querySelector('.test')
+
+  const data1 = await dataLoaderOf(element).fetchData()
+  const data2 = await dataLoaderOf(element).fetchNextData()
+
+  expect(data1).toEqual({ data: [], hasMore: false })
+  expect(data2).toEqual({ data: [], hasMore: false })
 })
