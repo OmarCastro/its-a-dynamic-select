6201b64e3ab6dd8ee72440229659738bcd7f277d
OmarCastro <omar.castro.360@gmail.com>
2025-12-30 22:00:11 +0000

    test: add link pagination test

--- src/features/data-loading/fetch-data.unit.spec.js
+++ src/features/data-loading/fetch-data.unit.spec.js
@@ -99,3 +99,37 @@ test('dataLoaderOf - fetching next data when hasMore is false returns an empty r
   expect(data1).toEqual({ data: [], hasMore: false })
   expect(data2).toEqual({ data: [], hasMore: false })
 })
+
+test('dataLoaderOf - uses link pagination when response contains a "Link" header', async ({ expect, dom, fetch }) => {
+  fetch.throwErrorOnNonMockedRequests()
+  const { body } = dom.document
+  body.innerHTML = '<div class="test" data-src="test"></div>'
+  const response1 = Response.json([{ id: '1', text: 'hello world' }])
+  response1.headers.append('Link', '<https//example.com/test?cursor=test_cursor>; rel="next"')
+  fetch.mock(/.*test/, response1)
+  const response2 = Response.json([{ id: '2', text: 'hello werl' }])
+
+  fetch.mock(/.*cursor=test_cursor/, response2)
+
+  const element = body.querySelector('.test')
+  const data1 = await dataLoaderOf(element).fetchData()
+  const data2 = await dataLoaderOf(element).fetchNextData()
+  const data3 = await dataLoaderOf(element).fetchNextData()
+
+  expect(data1).toEqual({
+    data: [{ id: '1', text: 'hello world' }],
+    hasMore: true,
+    navigationMode: 'link',
+    href: 'https//example.com/test?cursor=test_cursor',
+  })
+  expect(data2).toEqual({
+    data: [{ id: '2', text: 'hello werl' }],
+    hasMore: false
+  })
+  expect(data3).toEqual({ data: [], hasMore: false })
+
+  expect(fetch.fetchHistory.inputHrefs).toEqual([
+    'https://example.com/test',
+    'https//example.com/test?cursor=test_cursor'
+  ])
+})
