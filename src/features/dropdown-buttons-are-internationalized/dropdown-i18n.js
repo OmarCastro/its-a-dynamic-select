5eeb4c50fc79083b9c72112cef879d9b017c239e
OmarCastro <omar.castro.360@gmail.com>
2026-01-08 21:30:22 +0000

    src: add i18n labels for buttons

---
+++ src/features/dropdown-buttons-are-internationalized/dropdown-i18n.js
@@ -0,0 +1,36 @@
+/* eslint-disable @cspell/spellchecker */
+export const i18nButtonLabels = Object.freeze({
+  ok: {
+    default: 'OK',
+    translations: {
+      es: 'Aceptar',
+      ar: 'موافق',
+      hi: 'ठीक है',
+      tr: 'Tamam',
+      zh: '确定',
+      ko: '확인',
+    }
+  },
+
+  cancel: {
+    default: 'Cancel',
+    translations: {
+      es: 'Cancelar',
+      zh: '取消',
+      hi: 'रद्द करें',
+      ar: 'إلغاء',
+      tr: 'İptal',
+      fr: 'Annuler',
+      pt: 'Cancelar',
+      ru: 'Отмена',
+      id: 'Batal',
+      de: 'Abbrechen',
+      ja: 'キャンセル',
+      vi: 'Hủy',
+      ko: '취소',
+      it: 'Annulla',
+      pl: 'Anuluj',
+      uk: 'Скасувати',
+    }
+  }
+})
