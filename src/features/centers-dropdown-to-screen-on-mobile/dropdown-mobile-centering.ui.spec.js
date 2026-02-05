6865d299b265fc09e6aa69e91cf8de40f42982a5
OmarCastro <omar.castro.360@gmail.com>
2026-02-05 21:10:09 +0000

    test: add test page in the same folder as test file on
    On dropdown mobile centering

--- src/features/centers-dropdown-to-screen-on-mobile/dropdown-mobile-centering.ui.spec.js
+++ src/features/centers-dropdown-to-screen-on-mobile/dropdown-mobile-centering.ui.spec.js
@@ -1,23 +1,24 @@
-import { test } from '../../../test-utils/ui/test.util.js'
+import { test, expect } from '../../../test-utils/ui/test.util.js'
 import { devices } from '@playwright/test'
 /** @import { Page } from '@playwright/test' */
 
 test.use({
   ...devices['Galaxy S8']
 })
+
 /**
  * @param {Page} page
  */
-test('dynamic-select dropdown centers on mobile', async ({ page, expect }) => {
-  await page.goto('./build/docs/test-page.html#test-basic-select')
+test('dynamic-select dropdown centers on mobile', async ({ page, testPageLocation }) => {
+  await page.goto(testPageLocation + '#test-basic-select')
   const selectbox = page.getByRole('button').filter({ visible: true })
   await selectbox.click()
-  await expect.soft(await page.screenshot()).toMatchSnapshot('dropdown-centers-on-mobile.png')
+  await expect(page).toHaveScreenshot()
 })
 
-test('dynamic-select dropdown is scrollable if its content overflows the screen ', async ({ page, expect }) => {
-  await page.goto('./build/docs/test-page.html#test-group-select')
+test('dynamic-select dropdown is scrollable if its content overflows the screen ', async ({ page, testPageLocation }) => {
+  await page.goto(testPageLocation + '#test-group-select')
   const selectbox = page.getByRole('button').filter({ visible: true })
   await selectbox.click()
-  await expect.soft(await page.screenshot()).toMatchSnapshot('dropdown-scrollable-on-overflow.png')
+  await expect(page).toHaveScreenshot()
 })
--- test-utils/ui/test.util.js
+++ test-utils/ui/test.util.js
@@ -1,17 +1,24 @@
 /* eslint-disable no-empty-pattern */
-import { test as base, expect } from '@playwright/test'
-import { mkdir, writeFile } from 'node:fs/promises'
-import { join } from 'node:path'
+import { test as base, expect as baseExpect } from '@playwright/test'
+import { mkdir, writeFile, stat } from 'node:fs/promises'
+import { join, relative } from 'node:path'
+export { expect } from '@playwright/test'
+/**
+ * Get path stats, or null if it doesn't exist
+ * @param {import('node:fs').PathLike} path - path
+ */
+const statOrNull = (path) => stat(path).catch(() => null)
+const rootPath = process.cwd()
 
 export const test = base.extend({
   step: async ({}, use) => {
     await use(test.step)
   },
   expect: async ({}, use) => {
-    await use(expect)
+    await use(baseExpect)
   },
 
-  page: async ({ browserName, page }, use) => {
+  page: async ({ browserName, page }, use, testInfo) => {
     if (browserName !== 'chromium') {
       return await use(page)
     }
@@ -24,7 +31,7 @@ export const test = base.extend({
 
     const coverage = jsCoverage.map(entry => {
       const url = new URL(entry.url)
-      const scriptPath = `file://${process.cwd() + url.pathname}`
+      const scriptPath = `file://${rootPath}${url.pathname}`
       return {
         ...entry,
         url: scriptPath,
@@ -36,5 +43,16 @@ export const test = base.extend({
       const fileContent = JSON.stringify({ result: [entry] })
       await writeFile(filetoWrite, fileContent)
     }
+  },
+
+  testPageLocation: async ({}, use, testInfo) => {
+    const fileName = testInfo.file
+    const htmlFileName = fileName.replace(/\.js$/, '.html')
+    const stats = await statOrNull(htmlFileName)
+    if (stats) {
+      use(relative(rootPath, htmlFileName))
+      return
+    }
+    use('./build/docs/test-page.html')
   },
 })
