05bf3146d80bf85c19e7b6abc9740f6ae2f9fe7b
OmarCastro <omar.castro.360@gmail.com>
2026-02-19 22:16:06 +0000

    release: clean release artifacts

--- dist/esm/utils/object.js
+++
@@ -1,6 +0,0 @@
-/**
- *
- * @param {unknown} obj - target object
- * @returns {obj is Record<string,any>} true if `obj` is a plain object, false otherwise
- */
-export const isPlainObject = obj => !!obj && [null, Object.prototype].includes(Object.getPrototypeOf(obj))
